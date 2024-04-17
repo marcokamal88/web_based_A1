c900711e9a7ae55787fe4a27496bc69b88ee6fa3
OMNIA <101533216+omniamagdyy@users.noreply.github.com>
2024-04-18 01:39:52 +0200

    Client-side VALIDATIONs

--- validation.js
+++ validation.js
@@ -1,8 +1,17 @@
 inputs = document.getElementsByTagName("form")[0];
-password = document.getElementById("pwd");
-conPassword = document.getElementById("conpwd");
-const passwordRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&?])[A-Za-z\d!#$%&?]{8,}$/;
+const fullName = document.getElementById("fname");
+const userName  = document.getElementById("user");
+const birthdate = document.getElementById("Brithday");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+const password = document.getElementById("pwd");
+const conPassword = document.getElementById("conpwd");
+const registerButton = document.getElementById("btn");
+
+const emailRegex = /^\S+@\S+\.\S+$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&?])[A-Za-z\d!#$%&?]{8,}$/;
+const phoneRegex = /^(01)[0-9]{9}$/;
 
 console.log(inputs);
 function createError(errorMessage,index) {
@@ -16,18 +25,63 @@ function createError(errorMessage,index) {
         .getElementsByTagName("div")
         [index].appendChild(node);
 }
-function emptyFields() {
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value == "") {
-      console.log(inputs[i]);
-      createError('*this field is required',i);
+
+
+registerButton.addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    
+    const errorElements = document.querySelectorAll(".error");
+    errorElements.forEach(errorElement => errorElement.remove());
+
+    
+    const fields = [fullName, userName, birthdate, phone, email, address, password, conPassword];
+    let isValid = true;
+
+    fields.forEach((field, index) => {
+        const value = field.value.trim();
+        if (value === "") {
+            createError('* This field is required',index);
+            isValid = false;
+            changeBorderColor(field,false);
+
+        }else if (field === phone && !phoneRegex.test(value)) {
+          createError('* Please enter a valid phone number', index);
+          isValid = false;
+          changeBorderColor(field, false);
+
+        }
+        else if (field == email && !emailRegex.test(value)) {
+          //createError('*Please enter a vaild email address',index);
+          isValid = false;
+          changeBorderColor(field,false);
+          
+        }
+        else if (field == password && !passwordRegex.test(value)) {
+            createError('* Password must be at least 8 characters with at least 1 number and 1 special character',index);
+            isValid = false;
+            changeBorderColor(field,false);
+
+        } else if (field === conPassword && value !== password.value) {
+            createError('* Passwords do not match',index);
+            isValid = false;
+            changeBorderColor(field,false);
+
+        }
+        else {
+          changeBorderColor(field,true);
+        }
+    });
+    if (isValid) {
+        document.querySelector("form").submit();
     }
-    if (inputs[i] == password && !passwordRegex.test(password)) {
-        createError('*invalid password',i);
+    
+});
+function changeBorderColor(field,isValid){
+      if (isValid){
+        field.style.borderColor = "green";
+      }else{
+        field.style.borderColor = "red";
+      }
     }
-    if (inputs[i] == conPassword && conPassword.value!=password.value) {
-        createError('*password does not match',i);
-    }
-  }
-}
-emptyFields();
+    
